9a193e4b05300938176cce09eec76805d96b95f6
Jacob A. Nelson <jnelson@bgc-jena.mpg.de>
2021-11-02 15:19:10 +0100

    new R installation

--- install.R
+++ install.R
@@ -1,5 +1,3 @@
 repo = "http://cran.rstudio.com/"
-install.packages("devtools", repos=repo)
-install.packages("FME", repos=repo)
 install.packages("bigleaf", repos=repo)
 devtools::install_github("git@github.com:oscarperezpriego/ETpartitioning.git") 
